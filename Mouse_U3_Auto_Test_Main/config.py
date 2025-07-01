11ebe3ebdc026a711208ee23266b7f34492484a1
jasonkuo-0630 <189335532+jasonkuo-0630@users.noreply.github.com>
2025-07-01 18:05:13 +0800

    設定集(輪盤整合版)

--- Mouse_U3_Auto_Test_Main/config.py
+++ Mouse_U3_Auto_Test_Main/config.py
@@ -4,11 +4,12 @@
 # =============================================
 
 COM_PORT = "COM3"
+TURNTABLE_COM_PORT = "COM5"    # 輪盤控制 COM Port
 WS_IP = "192.168.0.225"
-WS_PORT = 8000
+WS_PORT = 8000                 # 小智 WebSocket Port
 
 # 測試點命名方式：支援格式化字串，例如 'A{:02d}'、'{:d}M' 等
-TEST_POINT_PATTERN = '{:d}m'  # 預設為 A01 ~ A10（可改為 '{:d}M' 則是 1M ~ 10M）
-TEST_POINT_RANGE = range(1, 6)  # 站點編號範圍
+TEST_POINT_PATTERN = '{:d}m'   # 預設為 1m ~ 5m（可改為 'A{:02d}' 則是 A01 ~ A05）
+TEST_POINT_RANGE = range(1, 6) # 站點編號範圍
 
 TEST_ROUNDS = 3  # 測試趟數（想要跑幾次 1~10 全部來回就改這邊）
